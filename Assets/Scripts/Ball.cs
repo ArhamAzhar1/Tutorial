737569033831dab037f1c5c47da571dee29db5a0
ArhamAzhar1 <arham8718290@gmail.com>
2024-08-01 18:09:31 +0500

    Scripts

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
-    // Start is called before the first frame update
+    float speed = 20f;
+    Rigidbody rigidbody;
+    Vector3 velocity;
+
     void Start()
     {
-        
+        rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = Vector3.down * _speed();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        rigidbody.velocity = Vector3.Reflect(rigidbody.velocity, collision.contacts[0]);
     }
 }
