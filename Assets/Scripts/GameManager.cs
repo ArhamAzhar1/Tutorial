0b2cf3805b63613056543675a151a9ca3aa98593
ArhamAzhar1 <arham8718290@gmail.com>
2024-08-09 18:28:45 +0500

    Designing panels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -16,15 +16,88 @@ public class GameManager : MonoBehaviour
     public GameObject levelCompletedPanel;
     public GameObject gameOverPanel;
 
+    public enum State {MENU, INIT, PLAY, LEVELCOMPLETED, LOADLEVEL, GAMEOVER }
+    State _state;
+
+    public void PlayClicked()
+    {
+        SwitchState(State.INIT);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        SwitchState(State.MENU);
     }
 
     // Update is called once per frame
+   
+
+    public void SwitchState(State newState)
+    {
+        EndState();
+        BeginState(newState);
+    }
+
+    void BeginState(State newState)
+    {
+        switch (newState)
+        {
+            case State.MENU:
+                panelMenu.SetActive(true);
+                break;
+            case State.INIT:
+                playPanel.SetActive(true);
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVELCOMPLETED:
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.GAMEOVER:
+                break;
+        }
+    }
+
     void Update()
     {
-        
+        switch (_state)
+        {
+            case State.MENU:
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVELCOMPLETED:
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.GAMEOVER:
+                break;
+        }
     }
+
+    void EndState()
+    {
+        switch (_state)
+        {
+            case State.MENU:
+                panelMenu.SetActive(false);
+                break;
+            case State.INIT:
+                break;
+            case State.PLAY:
+                break;
+            case State.LEVELCOMPLETED:
+                break;
+            case State.LOADLEVEL:
+                break;
+            case State.GAMEOVER:
+                playPanel.SetActive(false);
+                break;
+        }
+    }
+
 }
