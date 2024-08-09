683e6c92c6c4122d6d6cb433676db9f3d03c250c
ArhamAzhar1 <arham8718290@gmail.com>
2024-08-09 14:21:27 +0500

    Designed panels

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
     public GameObject ballPrefab;
     public GameObject playerPrefab;
+    public Text scoreText;
+    public Text ballsText;
+    public Text levelText;
+
+    public GameObject panelMenu;
+    public GameObject playPanel;
+    public GameObject levelCompletedPanel;
+    public GameObject gameOverPanel;
 
     // Start is called before the first frame update
     void Start()
